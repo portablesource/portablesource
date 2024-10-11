ac81940630ebaa72d2bbcee138a864be34d52afa
NeuroDonu <112660822+NeuroDonu@users.noreply.github.com>
2024-10-11 19:31:19 +0300

    add args for facefusion updater

--- portablesource/updater_facefusion.py
+++ portablesource/updater_facefusion.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import locale
 import winreg
+import argparse
 
 def get_localized_text(language, key):
     texts = {
@@ -70,6 +71,14 @@ abs_path = get_path_for_install()
 git = os.path.join(abs_path, "system", "git", "cmd", "git.exe")
 ff_obs = os.path.join(abs_path, "sources", "facefusion")
 python = os.path.join(abs_path, "sources", "facefusion", "venv", "Scripts", "python.exe")
+parser = argparse.ArgumentParser
+
+parser.add_argument('-wf', '--webcam-false', action='store_true', help="Выключить режим веб-камеры")
+parser.add_argument('-wt', '--webcam-true', action='store_true', help="Включить режим веб-камеры")
+parser.add_argument('-mb', '--master-branch', action='store_true', help="Лончить лишь мастер-ветку")
+parser.add_argument('-nb', '--next-branch', action='store_true', help="Лончить лишь бета-ветку")
+
+args = parser.parse_args()
 
 def run_git_command(args):
     subprocess.run([git] + args, check=True)
@@ -137,24 +146,50 @@ def ask_webcam_mode(language):
 
 def facefusion():
     language = get_system_language()
-    while True:
-        print(get_localized_text(language, "choose_action"))
-        print(get_localized_text(language, "update_master"))
-        print(get_localized_text(language, "update_next"))
-        choice = input(get_localized_text(language, "enter_choice")).strip()
-
-        if choice == '1':
-            update_branch("master")
-            webcam_mode = ask_webcam_mode(language)
-            uncensore("master")
-            start_ff("master", webcam_mode)
-        elif choice == '2':
-            update_branch("next")
-            uncensore("next")
-            webcam_mode = ask_webcam_mode(language)
-            start_ff("next", webcam_mode)
+    if args.master_branch:
+        update_branch("master")
+        uncensore("master")
+        if args.webcam_true:
+            webcam_mode = True
+        elif args.webcam_false:
+            webcam_mode = False
         else:
-            print(get_localized_text(language, "invalid_choice"))
+            webcam_mode = ask_webcam_mode(language)
+        
+        start_ff("master", webcam_mode)
+        return 
+    
+    if args.next_branch:
+        update_branch("next")
+        uncensore("next")
+        if args.webcam_true:
+            webcam_mode = True
+        elif args.webcam_false:
+            webcam_mode = False
+        else:
+            webcam_mode = ask_webcam_mode(language)
+        
+        start_ff("next", webcam_mode)
+        return 
+    else:
+        while True:
+            print(get_localized_text(language, "choose_action"))
+            print(get_localized_text(language, "update_master"))
+            print(get_localized_text(language, "update_next"))
+            choice = input(get_localized_text(language, "enter_choice")).strip()
+
+            if choice == '1':
+                update_branch("master")
+                webcam_mode = ask_webcam_mode(language)
+                uncensore("master")
+                start_ff("master", webcam_mode)
+            elif choice == '2':
+                update_branch("next")
+                uncensore("next")
+                webcam_mode = ask_webcam_mode(language)
+                start_ff("next", webcam_mode)
+            else:
+                print(get_localized_text(language, "invalid_choice"))
 
 if __name__ == "__main__":
     facefusion()
